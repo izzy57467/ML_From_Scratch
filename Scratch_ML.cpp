defecb4e416db5a5e64b6ff17fbfd4e979ac3bab
Isabelle Villegas <izzy57467@gmail.com>
2022-10-01 11:29:11 -0500

    Modified cpp file name and contents

--- Scratch_ML.cpp
+++ Scratch_ML.cpp
@@ -8,37 +8,27 @@
 
 using namespace std;
 
-void split(vector<int> survived, vector<int> sex){
-    // We know that the total data points are 1046. Do an 80/20 split based on this for train/test
-    //need to randomize the vectors we read in.
-    auto rng = default_random_engine{};
-    shuffle(survived, sex, rng);
-    //create new vectors for train and test.
-    vector<int>::const_iterator trainSur = survived.begin() + 800;
-    vector<int>::const_iterator testSur = survived.at(801) + survived.end();
-    vector<int>::const_iterator trainSex = sex.begin() + 800;
-    vector<int>::const_iterator testSex = sex.at(801) + sex.end();
-}
-
-
 int main(int argc, char** argv){
+    
     ifstream inFS; //Input file stream
     string line;
-    string survived_in, sex_in;
+    string heading_in,survived_in, sex_in, pclass_in;
     const int MAX_LEN = 1050;
+    vector<int> heading(MAX_LEN);
+    vector<int> pclass(MAX_LEN);
     vector<int> survived(MAX_LEN);
     vector<int> sex(MAX_LEN);
 
     //Try to open file
-    cout << "Opening file tiatanicLogReg.csv." << endl;
+    cout << "Opening file tiatanic_project.csv." << endl;
 
-    inFS.open("titanicLogReg.csv");
+    inFS.open("titanic_project.csv");
     if(!inFS.is_open()){
         cout << "Could not open file titanic_project.csv." << endl;
         return 1; //1 indicates error
     }
 
-    //Can now use inFS stream like cin stream
+        //Can now use inFS stream like cin stream
     //Boston.csv should contain two doubles
 
     cout << "Reading line 1" << endl;
@@ -47,17 +37,24 @@ int main(int argc, char** argv){
     //echo heading
     cout << "heading: " << line << endl;
 
+        //echo heading
+    cout << "heading: " << line << endl;
+
     int numObservations = 0;
     while (inFS.good()) {
+        getline(inFS, heading_in, ',');
+        getline(inFS, pclass_in, ',');
         getline(inFS, survived_in, ',');
         getline(inFS, sex_in, ',');
 
+        pclass.at(numObservations) = stof(pclass_in);
         survived.at(numObservations) = stof(survived_in);
         sex.at(numObservations) = stof(sex_in);
 
         numObservations++;
     }
-
+    
+    pclass.resize(numObservations);
     survived.resize(numObservations);
     sex.resize(numObservations);
 
@@ -68,5 +65,4 @@ int main(int argc, char** argv){
     inFS.close(); //Done with file, so close it
 
     cout << "Number of records: " << numObservations << endl;
-    
 }
